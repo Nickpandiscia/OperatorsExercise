dcaf582edd404af1237f4fee43b542a857a93d19
Nickpandiscia <npandiscia1@gmail.com>
2022-06-13 19:32:10 -0400

    initial commit

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var division = a / b;
+            var mod = a % b;
+
+            if (a == 17 && b == 4) ;
+            Console.WriteLine($"{a}/{b} is {b} remainder {mod}.");
+        }
+    }
+}
